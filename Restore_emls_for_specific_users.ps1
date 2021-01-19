36fb47ff014e74a52b567550fa5ea2af9920f894
IBeholderI <ras_atari@mail.ru>
2021-01-19 13:36:06 +0300

    Create Restore_emls_for_specific_users.ps1

---
+++ Restore_emls_for_specific_users.ps1
@@ -0,0 +1,6 @@
+#!/usr/bin/env pwsh
+$USERS = Read-Host -Prompt 'Specify users splitted by spaces, like "user1 user2" without quotes'
+$LIST = ("$USERS").split(' ')
+For ($i=0; $i -lt $LIST.Count; $i++) {
+    Invoke-Command { rsync -hrtzuvs --password-file=YOUR_PASSWORD_FILE --progress rsync@YOUR_BACKUP_SERVER:BACKUP_LOCATION/"$($LIST[$i])"/ /opt/kerio/mailserver/store/mail/YOUR_DOMAIN/"$($LIST[$i])" }
+}
