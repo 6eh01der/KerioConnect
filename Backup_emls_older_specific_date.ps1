78c1671ef540d8c98a575526fb43f9a511bfbd92
Artur Lebedev <ras_atari@mail.ru>
2021-11-26 09:27:41 +0300

    Update Backup_emls_older_specific_date.ps1

--- Backup_emls_older_specific_date.ps1
+++ Backup_emls_older_specific_date.ps1
@@ -1,5 +1,5 @@
 #!/usr/bin/env pwsh
-#https://github.com/IBeholderI/KerioConnect/blob/main/Backup_emls_older_specific_date.ps1
+#https://github.com/6eh01der/KerioConnect/blob/main/Backup_emls_older_specific_date.ps1
 $Day = Read-Host -Prompt 'Specify number of the day in a month, for example 17'
 $Month = Read-Host -Prompt 'Specify number of the month in a year, for example 3'
 $Year = Read-Host -Prompt 'Specify year, for example 2020'
