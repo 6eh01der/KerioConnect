cc50b9f9218a986c491198e226438e8f841ff3b6
Artur Lebedev <ras_atari@mail.ru>
2023-04-17 17:18:22 +0300

    Update Restore_emls_for_specific_users.ps1
    Everything to args

--- Restore_emls_for_specific_users.ps1
+++ Restore_emls_for_specific_users.ps1
@@ -2,6 +2,10 @@
 #https://github.com/6eh01der/KerioConnect/blob/main/Restore_emls_for_specific_users.ps1
 $USERS = Read-Host -Prompt 'Specify users splitted by spaces, like "user1 user2" without quotes'
 $LIST = ("$USERS").split(' ')
+$PATH = $args[0
+$DOMAIN = $args[1]
+$BACKUP_SERVER = $args[2]
+$BACKUP_LOCATION = $args[3]
 For ($i=0; $i -lt $LIST.Count; $i++) {
-    Invoke-Command { rsync -hrtzuvs --password-file=YOUR_PASSWORD_FILE --progress rsync@YOUR_BACKUP_SERVER:BACKUP_LOCATION/"$($LIST[$i])"/ /opt/kerio/mailserver/store/mail/YOUR_DOMAIN/"$($LIST[$i])" }
+    Invoke-Command { rsync -hrtzuvs --password-file=YOUR_PASSWORD_FILE --progress "rsync@$BACKUP_SERVER`:$BACKUP_LOCATION/$($LIST[$i])/" "$PATH/$DOMAIN/$($LIST[$i])" }
 }
