3ae5c6253eb964d76eb0d77a7b4eed1fb234f3c4
Artur Lebedev <ras_atari@mail.ru>
2023-04-17 17:09:52 +0300

    Update Remove_emls_older_specific_date_for_specific_users.ps1
    Path and domain as args

--- Remove_emls_older_specific_date_for_specific_users.ps1
+++ Remove_emls_older_specific_date_for_specific_users.ps1
@@ -5,8 +5,9 @@ $Month = Read-Host -Prompt 'Specify number of the month in a year, for example 3
 $Year = Read-Host -Prompt 'Specify year, for example 2020'
 $USERS = Read-Host -Prompt 'Specify users splitted by spaces, like "user1 user2" without quotes'
 $DATE = Get-Date -Year $Year -Month $Month -Day $Day
-$PATH = '/opt/kerio/mailserver/store/mail/YOUR_DOMAIN'
+$PATH = $args[0]
+$DOMAIN = $args[1]
 $LIST = ("$USERS").split(' ')
 For ($i=0; $i -lt $LIST.Count; $i++) {
-    Get-ChildItem "$PATH/$($LIST[$i])/*" -Exclude '#assoc', '__*', 'Journal' | Get-ChildItem -Include *.eml -Recurse | Where CreationTime -lt  $DATE | Remove-Item -Recurse
+    Get-ChildItem "$PATH/$DOMAIN/$($LIST[$i])/*" -Exclude '#assoc', '__*', 'Journal' | Get-ChildItem -Include *.eml -Recurse | Where CreationTime -lt  $DATE | Remove-Item -Recurse
 }
