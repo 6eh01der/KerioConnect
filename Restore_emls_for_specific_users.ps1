ffb43cdcc5ea7ed14abb4e7c4e53752f1cd5d0f6
Artur Lebedev <ras_atari@mail.ru>
2023-04-17 17:21:12 +0300

    Update Restore_emls_for_specific_users.ps1
    Missed bracket

--- Restore_emls_for_specific_users.ps1
+++ Restore_emls_for_specific_users.ps1
@@ -2,7 +2,7 @@
 #https://github.com/6eh01der/KerioConnect/blob/main/Restore_emls_for_specific_users.ps1
 $USERS = Read-Host -Prompt 'Specify users splitted by spaces, like "user1 user2" without quotes'
 $LIST = ("$USERS").split(' ')
-$PATH = $args[0
+$PATH = $args[0]
 $DOMAIN = $args[1]
 $BACKUP_SERVER = $args[2]
 $BACKUP_LOCATION = $args[3]
