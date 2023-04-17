d1dddc23ed0caa7193508b20e4b7a673d3260833
Artur Lebedev <ras_atari@mail.ru>
2023-04-17 17:23:39 +0300

    Update Backup_emls_older_specific_date.ps1
    Backup parameters as args

--- Backup_emls_older_specific_date.ps1
+++ Backup_emls_older_specific_date.ps1
@@ -7,6 +7,9 @@ $USERS = Read-Host -Prompt 'Specify users splitted by spaces, like "user1 user2"
 $DATE = Get-Date -Year $Year -Month $Month -Day $Day
 $PATH = $args[0]
 $DOMAIN = $args[1]
+$BACKUP_SERVER = $args[2]
+$BACKUP_LOCATION = $args[3]
+$BACKUP_USER = $args[4]
 If ($USERS -ne "") {
 $LIST = ("$USERS").split(' ')
 } Else {
@@ -19,5 +22,5 @@ $EML_LIST = For ($i=0; $i -lt $EML_LIST.Count; $i++) {
     $($EML_LIST.FullName[$i]).replace("$PATH/", '')
 }
 For ($i=0; $i -lt $EML_LIST.Count; $i++) {
-    Invoke-Command { rsync -hrRtluvpogs --password-file=YOUR_PASSWORD_FILE --progress "$PATH/./$($EML_LIST[$i])" YOUR_USER@YOUR_BACKUP_SERVER:/BACKUP_LOCATION/
+    Invoke-Command { rsync -hrRtluvpogs --password-file=YOUR_PASSWORD_FILE --progress "$PATH/./$($EML_LIST[$i])" "$BACKUP_USER@$BACKUP_SERVER`:$BACKUP_LOCATION/"
 }
